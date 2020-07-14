49e3c087e0d3515576297b9f34f7b44a4174ea93
LEEJEEWON <60544994+leejiwon6315@users.noreply.github.com>
2020-07-14 21:11:38 +0900

    Update and rename spread 예제.js to spread & rest 예제.js

---
+++ JavaScript/spread & rest 예제.js
@@ -0,0 +1,39 @@
+//...spread
+const student = {
+	name : '이지원'
+}
+
+const univStudent = {
+	...student,
+	major : '글로벌미디어학부'
+}
+
+const koreanUnivStudent = {
+	...univStudent,
+	region: 'Seoul'
+}
+
+console.log(koreanUnivStudent);
+
+const oddNumbers = [1, 3, 5, 7, 9];
+const evenNumbers = [2, 4, 6, 8, 10];
+const allNumbers = [...oddNumbers, ...evenNumbers];
+
+function allNumbersFunction(...restNum){
+	//...rest
+	console.log(restNum);
+}
+
+const result = allNumbersFunction(...allNumbers)
+
+//destructuring
+const SoondilUnivStudent = {
+	name : '이지원',
+	major : '글로벌미디어학부',
+	region : '서울'
+};
+
+const { name, ...rest } = SoondilUnivStudent;
+	   //rest를 활용하여 name을 제외한 다른 요소들을 부분집합으로 만듬
+	   
+console.log(rest);
