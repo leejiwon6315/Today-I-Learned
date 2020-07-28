99f9f8476af116b64e3b90176e3705c17b33e592
LEEJEEWON <60544994+leejiwon6315@users.noreply.github.com>
2020-07-28 15:58:23 +0900

    Update [이름 출력 예제] index.js

--- React/[이름 출력 예제] index.js
+++ React/[이름 출력 예제] index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 const formatName = function (name){
     return `${name.firstName} ${name.lastName}`;
