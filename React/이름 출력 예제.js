652392d14b5d02153139feef4fa53f5e34fe60b5
LEEJEEWON <60544994+leejiwon6315@users.noreply.github.com>
2020-07-28 15:49:13 +0900

    Update and rename React/JSX/이름 출력 예제.js to React/이름 출력 예제.js

--- React/이름 출력 예제.js
+++ React/이름 출력 예제.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const formatName = function (name){
     return `${name.firstName} ${name.lastName}`;
