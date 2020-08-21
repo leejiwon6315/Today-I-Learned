735bd92d4550bc6eed510d3a207cf474c21f7eb5
LEEJEEWON <leejiwon6315@naver.com>
2020-08-21 14:07:43 +0900

    Create [Hooks - useEffect로 Life Cycle 구현 예제] App.js

---
+++ React/[Hooks - useEffect로 Life Cycle 구현 예제] App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import CountButton from "./components/CountButton";
+import Number from "./components/Number";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100px;
+  margin-top: 100px;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 50px;
+`;
+
+const App = () => {
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect -> componentDidMount");
+
+    return () => {
+      console.log("useEffect -> componentWillUnmount");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(`componentDidUpdate (number) -> ${number}`);
+  }, [number]);
+
+  useEffect(() => {
+    console.log("useEffect -> componentDidUpdate");
+  });
+
+  return (
+    <Wrapper>
+      <ButtonWrapper>
+        <CountButton onClick={() => setNumber(number + 1)} text="+" />
+        <CountButton onClick={() => setNumber(number - 1)} text="-" />
+      </ButtonWrapper>
+      <Number number={number} />
+    </Wrapper>
+  );
+};
+
+export default App;
