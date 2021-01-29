347f9547239c80bb051b8bedd60ccd3da591b24f
LEEJEEWON <leejiwon6315@naver.com>
2021-01-30 00:40:04 +0900

    interface 사용 예제

--- TypeScript/ex2.ts
+++ TypeScript/ex2.ts
@@ -11,8 +11,6 @@ const person = {
 };
 
 const sayHi = (person: Human): string => {
-  // 변수들의 타입을 직접 지정해주기 + 함수가 반환할 타입을 설정
-  // ?는 선택적인 요소를 넣을 때 사용
   return `name: ${person.name} age: ${person.age} gender: ${person.gender}`;
 };
 
